7e56b085973d5389d955006145f6f5888acfef4a
Kamil Richert <krichert@atlassian.com>
2023-05-15 12:07:46 +0200

    Destructuring part 2

--- react/my-app/src/Contact.js
+++ react/my-app/src/Contact.js
@@ -1,16 +1,16 @@
 import './Contact.css';
 
 export function Contact(props) {
-    const { data } = props; 
+    const { data: { phone, address, email } } = props; 
 
     return (
         <div className="contact-container">
             <h3>TEL</h3>
-            <p>{data.phone}</p>
+            <p>{phone}</p>
             <h3>ADRES</h3>
-            <p>{data.address.street} {data.address.number}, {data.address.city}</p>
+            <p>{address.street} {address.number}, {address.city}</p>
             <h3>EMAIL</h3>
-            <p>{data.email}</p>
+            <p>{email}</p>
         </div>
     )
 }
