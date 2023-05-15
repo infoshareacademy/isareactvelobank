cb46b4930dc1e69bf35a49397f998df23a5dbe88
Kamil Richert <krichert@atlassian.com>
2023-05-15 14:09:51 +0200

    Refactor with creating about me component

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,27 +1,10 @@
 import Wrapper from './Wrapper';
-import MyName from './MyName';
-import { MyFavouriteDishes } from './MyFavouriteDishes';
-import { Contact } from './Contact';
-
-const dishes = [
-     { id: 1, name: 'Kebab' },
-     { id: 2, name: 'Zupa pomidorowa' },
-     { id: 3, name: 'Homar' },
-]
+import AboutMe from './about-me/AboutMe';
 
 function App() {
   return (
     <Wrapper>
-        <MyName name="Kamil" />
-        <MyName name="Kamil" surname="Richert" />
-        <Contact data={
-            {
-                phone: '111 222 333',
-                address: { street: 'Słowackiego', city: 'Gdańsk', number: 37 }, 
-                email: 'moj@mail.com'
-            }
-        } />
-        <MyFavouriteDishes dishes={dishes} />
+        <AboutMe />
     </Wrapper>
   );
 }
---
+++ react/my-app/src/about-me/AboutMe.js
@@ -0,0 +1,28 @@
+import MyName from './MyName';
+import { MyFavouriteDishes } from './MyFavouriteDishes';
+import { Contact } from './Contact';
+
+const dishes = [
+    { id: 1, name: 'Kebab' },
+    { id: 2, name: 'Zupa pomidorowa' },
+    { id: 3, name: 'Homar' },
+]
+
+const AboutMe = () => {
+    return (
+        <>
+            <MyName name="Kamil" />
+            <MyName name="Kamil" surname="Richert" />
+            <Contact data={
+                {
+                    phone: '111 222 333',
+                    address: { street: 'Słowackiego', city: 'Gdańsk', number: 37 }, 
+                    email: 'moj@mail.com'
+                }
+            } />
+            <MyFavouriteDishes dishes={dishes} />
+        </>
+    )
+}
+
+export default AboutMe;
