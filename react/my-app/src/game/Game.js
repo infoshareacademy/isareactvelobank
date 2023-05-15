dde972366ac1ce05754eed561f20b01b7e2843e2
Kamil Richert <krichert@atlassian.com>
2023-05-15 15:20:37 +0200

    Added manipulating local state in game

--- react/my-app/src/game/Game.js
+++ react/my-app/src/game/Game.js
@@ -1,14 +1,27 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Score = styled.span`
+    color: ${props => props.isRed ? 'red' : 'black'}
+`;
 
 export const Game = ({ name }) => {
     const [points, setPoints] = useState(0);
 
+    const increase = () => {
+        setPoints(points + 5);
+    }
+
+    const decrease = () => {
+        setPoints(points - 5);
+    }
+
     return <>
         <h2>Witaj w grzę {name}!</h2>
-        <h2>Twoja liczba punków to: {points}!</h2>
+        <h2>Twoja liczba punków to: <Score isRed={points < 0}>{points}</Score>!</h2>
         <div>
-            <button>+</button>
-            <button>-</button>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
         </div>
     </>
 }
