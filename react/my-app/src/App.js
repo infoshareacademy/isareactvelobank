237dbdabba9be46b47fc443a2e797417d700fd43
Kamil Richert <krichert@atlassian.com>
2023-05-15 12:00:59 +0200

    Contact as example of styling

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,10 +1,18 @@
 import MyName from './MyName';
+import { Contact } from './Contact';
 
 function App() {
   return (
     <>
         <MyName name="Kamil" />
         <MyName name="Kamil" surname="Richert" />
+        <Contact data={
+            {
+                phone: '111 222 333',
+                address: { street: 'Słowackiego', city: 'Gdańsk', number: 37 }, 
+                email: 'moj@mail.com'
+            }
+        } />
     </>
   );
 }
