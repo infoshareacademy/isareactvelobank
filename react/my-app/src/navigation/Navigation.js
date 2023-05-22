825fe99431d465e9eacb32e637c1bfbff5b10e35
Kamil Richert <krichert@atlassian.com>
2023-05-22 09:53:27 +0200

    Used Navlink instead of Link

--- react/my-app/src/navigation/Navigation.js
+++ react/my-app/src/navigation/Navigation.js
@@ -1,7 +1,13 @@
-import { Link } from 'react-router-dom';
+import { NavLink as RouterLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
+const getActiveStyles = ({ isActive }) => {
+    return {
+        textDecoration: isActive ? 'underline' : undefined
+    }
+}
 
 export const Navigation = () => {
     return (
@@ -9,11 +15,11 @@ export const Navigation = () => {
         <Container>
           <Navbar.Brand>My App</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about-me">About me</Nav.Link>
-            <Nav.Link as={Link} to="/game">Game</Nav.Link>
-            <Nav.Link as={Link} to="/form">Form</Nav.Link>
-            <Nav.Link as={Link} to="/users">Users</Nav.Link>
+            <Nav.Link as={RouterLink} style={getActiveStyles} to="/">Home</Nav.Link>
+            <Nav.Link as={RouterLink} style={getActiveStyles} to="/about-me">About me</Nav.Link>
+            <Nav.Link as={RouterLink} style={getActiveStyles} to="/game">Game</Nav.Link>
+            <Nav.Link as={RouterLink} style={getActiveStyles} to="/form">Form</Nav.Link>
+            <Nav.Link as={RouterLink} style={getActiveStyles} to="/users">Users</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
