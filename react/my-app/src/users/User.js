69710debd6e6f2a94920ef8ce771532dee0ee257
Kamil Richert <krichert@atlassian.com>
2023-05-23 08:50:43 +0200

    Changed link version not to be absolute path

--- react/my-app/src/users/User.js
+++ react/my-app/src/users/User.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const User = ({ user }) => {
     return (
         <li>
-            <Link to={`/users/${user.id}`}>{user.name}</Link> works in 
+            <Link to={`${user.id}`}>{user.name}</Link> works in 
             <span title={user.company.catchPhrase}> {user.company.name}</span>
         </li>
     )
