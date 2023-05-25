539b655f91e34f5354a011e3bc93efa5dae13b78
Kamil Richert <krichert@atlassian.com>
2023-05-25 09:38:48 +0200

    Change use effect to use query in menu

--- react/burgers-app/src/content/menu/index.tsx
+++ react/burgers-app/src/content/menu/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,43 +6,47 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import CircularProgress from '@mui/material/CircularProgress';
 import { PageWrapper } from "../../common/page-wrapper";
-import { Burger, BurgerResponse } from '../../common/types';
+import { Burger } from '../../common/types';
+import { getBurgers } from '../../services/burgers';
 
 export const Menu = () => {
-    const [burgers, setBurgers] = useState<Burger[]>([]);
+    const { data: burgers, error, isLoading } = useQuery<Burger[], Error>('burgers', getBurgers);
 
-    useEffect(() => {
-        fetch('https://rest-api-b6410.firebaseio.com/burgers.json')
-            .then(r => r.json())
-            .then((data: BurgerResponse) => {
-                const formattedData = Object.keys(data).map(key => ({ id: key, ...data[key] }));
-                setBurgers(formattedData);
-            })
-    }, []);
+    if (error) {
+        return <PageWrapper title="An error occurred"> 
+            {error.message}
+        </PageWrapper>
+    }
 
     return (
         <PageWrapper title="Menu">
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="right">Ingredients</TableCell>
-                        <TableCell align="right">Price</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {burgers.map((burger) => (
-                            <TableRow key={burger.id}>
-                                <TableCell>{burger.name}</TableCell>
-                                <TableCell align="right">{burger.ingredients}</TableCell>
-                                <TableCell align="right">{burger.price}</TableCell>
+            {isLoading 
+                ? <CircularProgress />
+                : (
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right">Ingredients</TableCell>
+                                <TableCell align="right">Price</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                            </TableHead>
+                            <TableBody>
+                                {burgers?.map((burger) => (
+                                        <TableRow key={burger.id}>
+                                            <TableCell>{burger.name}</TableCell>
+                                            <TableCell align="right">{burger.ingredients}</TableCell>
+                                            <TableCell align="right">{burger.price}</TableCell>
+                                        </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )
+            }
         </PageWrapper>
     )
 }
---
+++ react/burgers-app/src/services/burgers/index.ts
@@ -0,0 +1,8 @@
+import { BurgerResponse, Burger } from "../../common/types";
+
+export const getBurgers = async (): Promise<Burger[]> => {
+    const r = await fetch('https://rest-api-b6410.firebaseio.com/burgers.json');
+    const data: BurgerResponse = await r.json();
+    const formattedData = Object.keys(data).map(key => ({ id: key, ...data[key] }));
+    return formattedData;
+}
