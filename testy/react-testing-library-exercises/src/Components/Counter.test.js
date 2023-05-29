2269e13b89726b6b889d0bda92ecc2b469704f7d
Kamil Richert <krichert@atlassian.com>
2023-05-29 11:02:27 +0200

    React testing library

--- testy/react-testing-library-exercises/src/Components/Counter.test.js
+++ testy/react-testing-library-exercises/src/Components/Counter.test.js
@@ -4,18 +4,47 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('Counter', () => {
   test('should have initial counter set to 0', () => {
+    // arrange
+    render(<Counter />);
 
+    // act
+    const element = screen.getByText('value: 0');
+
+    // assert
+    expect(element).toBeInTheDocument();
   });
 
   test('should have increase counter when increase clicked', () => {
+    render(<Counter />);
 
+    const button = screen.getByRole('button', { name: 'increase'});
+    fireEvent.click(button);
+    const element = screen.getByText('value: 1');
+
+    expect(element).toBeInTheDocument();
   });
 
   test('should have decrease counter when decrease clicked', () => {
+    render(<Counter />);
 
+    const button = screen.getByRole('button', { name: 'decrease'});
+    fireEvent.click(button);
+    const element = screen.getByText('value: -1');
+
+    expect(element).toBeInTheDocument();
   });
 
   test('should have reset counter when reset clicked', () => {
+    render(<Counter />);
 
+    const increaseButton = screen.getByRole('button', { name: 'increase'});
+    fireEvent.click(increaseButton);
+    const elementWithValue1 = screen.getByText('value: 1');
+    expect(elementWithValue1).toBeInTheDocument();
+
+    const resetButton = screen.getByRole('button', { name: 'reset'});
+    fireEvent.click(resetButton);
+    const elementWithValue0 = screen.getByText('value: 0');
+    expect(elementWithValue0).toBeInTheDocument();
   });
 });
--- testy/react-testing-library-exercises/src/Components/TextTransformer.test.js
+++ testy/react-testing-library-exercises/src/Components/TextTransformer.test.js
@@ -1,27 +1,48 @@
 // Napisz podane testy!
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, fireEvent, getByText } from '@testing-library/react';
 
 import TextTransformer from './TextTransformer';
 
 describe('TextTransformer', () => {
   test('renders correctly', () => {
-    // toMatchSnapshot
-
+    const element = render(<TextTransformer />)
+    // TODO FIX ME
+    // expect(element).toMatchSnapshot();
   });
   test('should transform inserted text', () => {
+    render(<TextTransformer />)
 
+    const input = screen.getByLabelText('Insert text');
+    fireEvent.change(input, {target: {value: 'velobank'}});
+    const element = screen.getByText('__VELOBANK__');
+
+    expect(element).toBeInTheDocument();
   });
 
   test('should not override inserted text when prop has been set later', () => {
+    const { rerender } = render(<TextTransformer />)
 
+    const input = screen.getByLabelText('Insert text');
+    fireEvent.change(input, {target: {value: 'velobank'}});
+    rerender(<TextTransformer value="Propstext" />);
+    const element = screen.getByText('__VELOBANK__');
+
+    expect(element).toBeInTheDocument();
   });
 
   test('should transform text from props when prop has been set', () => {
+    render(<TextTransformer value="Propstext" />)
 
+    const element = screen.getByText('__PROPSTEXT__');
+
+    expect(element).toBeInTheDocument();
   });
 
   test('should not show transformed text when state is empty', () => {
+    render(<TextTransformer />);
 
+    const element = screen.queryByText('Transformed text');
+
+    expect(element).not.toBeInTheDocument();
   })
 });
